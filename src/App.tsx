2c99923ba1df4ceffd668c881fca7fea68fe2aee
Lavish Patel <patellavish.9336@gmail.com>
2025-06-01 15:52:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";webkitURL
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Landing from "./pages/Landing";
 import BlogPage from "./pages/BlogPage";
@@ -22,7 +22,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-{/*           <Route path="/landing" element={<Landing />} /> */}
+          <Route path="/landing" element={<Landing />} />
           <Route path="/blog" element={<BlogPage />} />
           <Route path="/roadmap-selection" element={<RoadmapSelectionPage />} />
           <Route path="/roadmap" element={<RoadmapPage />} />
